8b89a79c25866cf6fb4a241d4989f97b6ceb9e1b
Kelvis Antonio Rodrigues Borges <kelvis.borges@ms.senac.br>
2021-02-11 00:10:48 -0400

    Adiciona Models Seguro e Marca do Carro

---
+++ src/app/shared/models/seguro.ts
@@ -0,0 +1,10 @@
+import { MarcaCarro } from './marca-carro';
+
+export class Seguro {
+    marcaCarro: MarcaCarro;
+    modeloCarro: string;
+    placaCarro: string;
+    nomeProprietario: string;
+    sobrenomeProprietario: string;
+    dataNascimentoProprietario: string;
+}
